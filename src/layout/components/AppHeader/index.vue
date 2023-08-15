73a61c17e6b697c4ed36de6d3c406dc391373555
lixiang <Xiang.Li2@externals.adidas.com>
2023-08-15 18:52:22 +0800

    update

--- src/layout/components/AppHeader/index.vue
+++ src/layout/components/AppHeader/index.vue
@@ -3,16 +3,25 @@
     <el-row justify="space-between" align="middle" class="c-app-header">
         <el-col :span="12">
             <router-link to="/" class="c-header__aside">
-                <div class="c-header__title">POS Lifecycle Management</div>
+                <div class="c-header__title">Vite Vue3</div>
             </router-link>
         </el-col>
         <el-col class="c-app-header__user" :span="12">
-            <span class="c-app-header__user__name">User name</span>
-            <el-avatar fit="cover" class="c-app-header__user__avatar">
-                <el-icon size="20">
-                    <i-ep-UserFilled />
-                </el-icon>
-            </el-avatar>
+            <el-dropdown>
+                <el-avatar :size="30" class="c-app-header__user__avatar">
+                    <el-icon :size="20">
+                        <i-ep-UserFilled />
+                    </el-icon>
+                </el-avatar>
+                <template #dropdown>
+                    <el-dropdown-menu>
+                        <el-dropdown-item>User name</el-dropdown-item>
+                        <el-dropdown-item>English</el-dropdown-item>
+                        <el-dropdown-item>简体中文</el-dropdown-item>
+                        <el-dropdown-item>退出登录</el-dropdown-item>
+                    </el-dropdown-menu>
+                </template>
+            </el-dropdown>
         </el-col>
     </el-row>
 </template>
@@ -22,18 +31,18 @@
     // background-color: red;
     width: 100%;
     height: 60px;
-    padding: 0 20px;
+    padding: 0 30px;
 
     &__user {
         text-align: right;
 
-        &__name {
-            margin-right: 20px;
-            font-weight: bold;
-        }
+        // &__name {
+        //     margin-right: 20px;
+        //     font-weight: bold;
+        // }
 
         &__avatar {
-            background-color: #00a2f0;
+            // background-color: #00a2f0;
             cursor: pointer;
         }
     }
