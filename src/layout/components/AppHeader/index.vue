0ed7d92fbd088260bbc50a751a54c0b288f49515
lixiang <Xiang.Li2@externals.adidas.com>
2023-08-15 16:33:21 +0800

    fixed bug

--- src/layout/components/AppHeader/index.vue
+++ src/layout/components/AppHeader/index.vue
@@ -7,23 +7,35 @@
             </router-link>
         </el-col>
         <el-col class="c-app-header__user" :span="12">
-            <span>User name</span>
-            <el-icon>
-                <i-ep-plus />
-            </el-icon>
+            <span class="c-app-header__user__name">User name</span>
+            <el-avatar fit="cover" class="c-app-header__user__avatar">
+                <el-icon size="20">
+                    <i-ep-UserFilled />
+                </el-icon>
+            </el-avatar>
         </el-col>
     </el-row>
 </template>
 <style lang="scss" scoped>
 @import '@/styles/variables.scss';
 .c-app-header {
-    background-color: red;
+    // background-color: red;
     width: 100%;
     height: 60px;
     padding: 0 20px;
 
     &__user {
         text-align: right;
+
+        &__name {
+            margin-right: 20px;
+            font-weight: bold;
+        }
+
+        &__avatar {
+            background-color: #00a2f0;
+            cursor: pointer;
+        }
     }
 }
 </style>
